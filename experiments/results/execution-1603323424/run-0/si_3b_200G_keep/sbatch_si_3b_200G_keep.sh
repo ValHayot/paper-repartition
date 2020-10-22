9637646bcef4016cc465e8cf11e7c91ba4c258a0
valhayot <valeriehayot@gmail.com>
2020-10-22 07:57:11 -0400

    added first set of results

---
+++ experiments/results/execution-1603323424/run-0/si_3b_200G_keep/sbatch_si_3b_200G_keep.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --account=vhs
+#SBATCH --job-name=si_3b_200G_keep
+#SBATCH --nodes=1
+#SBATCH --mem=200000
+#SBATCH --output=/home/vhs/paper-repartition/experiments/results/execution-1603323424/run-0/si_3b_200G_keep/slurm-%x-%j.out
+
+
+rm -rf /disk0/vhs/repartition
+mkdir /disk0/vhs/repartition
+cd /disk0/vhs/repartition
+
+
+echo "Clearing cache" && sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+source /home/vhs/paper-repartition/.venv/bin/activate
+export KEEP_LOG=/home/vhs/paper-repartition/experiments/results/execution-1603323424/run-0/si_3b_200G_keep/logs.csv
+
+
+repartition --max-mem 200000000000 --create  "(3500, 3500, 3500)" "(700, 700, 700)" "(350, 350, 350)" keep
+
+/usr/bin/time -o /home/vhs/paper-repartition/experiments/results/execution-1603323424/run-0/si_3b_200G_keep/runtime.txt -v repartition --max-mem 200000000000 --repartition "(3500, 3500, 3500)" "(700, 700, 700)" "(350, 350, 350)" keep
+repartition --max-mem 200000000000 --delete "(3500, 3500, 3500)" "(700, 700, 700)" "(350, 350, 350)" keep
+
+
+echo "Removing directories"
+rm -rf /disk0/vhs/repartition
---
+++ experiments/results/execution-1603323424/run-0/si_6b_200G_baseline/sbatch_si_6b_200G_baseline.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --account=vhs
+#SBATCH --job-name=si_6b_200G_baseline
+#SBATCH --nodes=1
+#SBATCH --mem=200000
+#SBATCH --output=/home/vhs/paper-repartition/experiments/results/execution-1603323424/run-0/si_6b_200G_baseline/slurm-%x-%j.out
+
+
+rm -rf /disk0/vhs/repartition
+mkdir /disk0/vhs/repartition
+cd /disk0/vhs/repartition
+
+
+echo "Clearing cache" && sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+source /home/vhs/paper-repartition/.venv/bin/activate
+export KEEP_LOG=/home/vhs/paper-repartition/experiments/results/execution-1603323424/run-0/si_6b_200G_baseline/logs.csv
+
+
+repartition --max-mem 200000000000 --create  "(3500, 3500, 3500)" "(175, 175, 175)" "(350, 350, 350)" baseline
+
+/usr/bin/time -o /home/vhs/paper-repartition/experiments/results/execution-1603323424/run-0/si_6b_200G_baseline/runtime.txt -v repartition --max-mem 200000000000 --repartition "(3500, 3500, 3500)" "(175, 175, 175)" "(350, 350, 350)" baseline
+repartition --max-mem 200000000000 --delete "(3500, 3500, 3500)" "(175, 175, 175)" "(350, 350, 350)" baseline
+
+
+echo "Removing directories"
+rm -rf /disk0/vhs/repartition
